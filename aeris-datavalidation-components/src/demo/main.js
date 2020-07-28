5195a846c1b870d05d2718d0d6b48f967866c8fc
dkande <desse.kande@ipsl.fr>
2020-07-28 13:04:53 +0200

    Init aeris-datavalidation-components default struct

---
+++ aeris-datavalidation-components/src/demo/main.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+
+import Vuex from "vuex";
+import VueRouter from "vue-router"
+import vuetify from './../../plugins/vuetify'
+import app from "./App.vue";
+import catalogue from "./modules/aeris-datavalidation-ui/submodules/catalogue-ground-data";
+import HelloWorld from "./modules/aeris-datavalidation-ui/submodules/HelloWorld";
+
+Vue.use(Vuex);
+Vue.use(VueRouter);
+Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  namespaced: true,
+  modules: {}
+});
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/catalogue",
+      component: () => catalogue
+    },
+    {
+      path: "/statistics",
+      component: () => HelloWorld
+    },
+  ]
+});
+
+new Vue({
+  el: "#app",
+  router,
+  store,
+  vuetify,
+  template: "<app/>",
+  components: {
+    app
+  }
+}).$mount("#app");
---
+++ aeris-datavalidation-components/src/demo/modules/aeris-datavalidation-ui/submodules/catalogue-ground-data.vue
@@ -0,0 +1,8 @@
+<template>
+    <h3>Ground data page</h3>
+</template>
+<script>
+    export default {
+        name: 'catalogue-ground-data.vue',
+    };
+</script>
