bbc4a0e62b6ad75e01fca58705443066c43392ba
dkande <desse.kande@ipsl.fr>
2020-07-29 15:47:52 +0200

    Added FlagService flag

--- data-validation-rest/src/main/java/com/aeris/datavalidationrest/catalogue/flag/FlagService.java
+++ data-validation-rest/src/main/java/com/aeris/datavalidationrest/catalogue/flag/FlagService.java
@@ -7,13 +7,8 @@ import org.springframework.web.client.RestTemplate;
 public class FlagService {
     public static final String SEDOO_FLAGS_DATA_URL = "https://sedoo.aeris-data.fr/actris-datacenter-rest/rest/quality/flags/91440f71-9c3e-5d31-befc-2729873ce581";
 
-    public Flag[] getFlag() {
-        RestTemplate restTemplate = new RestTemplate();
+    public Flag[] getFlag(RestTemplate restTemplate) {
         Flag[] flags =  restTemplate.getForObject( SEDOO_FLAGS_DATA_URL, Flag[].class);
-        System.out.println("Test flag service : ");
-        for(Flag flag : flags) {
-            System.out.println(flag);
-        }
         return flags;
     }
 }
--- data-validation-rest/src/test/java/com/aeris/datavalidationrest/catalogue/FlagServiceTest.java
+++ data-validation-rest/src/test/java/com/aeris/datavalidationrest/catalogue/FlagServiceTest.java
@@ -1,18 +1,58 @@
 package com.aeris.datavalidationrest.catalogue;
 
+import com.aeris.datavalidationrest.catalogue.data.GroundData;
+import com.aeris.datavalidationrest.catalogue.flag.Flag;
+import com.aeris.datavalidationrest.catalogue.flag.FlagDao;
 import com.aeris.datavalidationrest.catalogue.flag.FlagService;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class FlagServiceTest {
+    @Autowired
+    private FlagDao flagDao;
+    @Autowired
+    private RestTemplate restTemplate;
+
     private final FlagService flagService = new FlagService();
 
     @Test
+    @Order(1)
     public void getDataInfo() {
-        flagService.getFlag();
+        Flag[] flags = flagService.getFlag(restTemplate);
+        Assert.assertTrue( 0 < flags.length  );
+    }
+
+    @Test
+    @Order(2)
+    public void deleteAllFlags() {
+        List<Flag> flags;
+        flagDao.deleteAll();
+        flags = flagDao.findAll();
+        Assert.assertTrue(flags.isEmpty());
+    }
+
+    @Test
+    @Order(3)
+    public void initGroundData() {
+        List<Flag> dbFlags;
+        Flag[] flags = flagService.getFlag(restTemplate);
+        for( Flag flag: flags ) {
+            flagDao.save(flag);
+        }
+        dbFlags = flagDao.findAll();
+        Assert.assertFalse(dbFlags.isEmpty());
     }
 }
