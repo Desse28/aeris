406e46cc3f44ca799f44399a81629806f261c8db
dkande <desse.kande@ipsl.fr>
2020-08-11 11:10:18 +0200

    Init InstrumentResource methods

--- data-validation-rest/src/main/java/com/aeris/datavalidationrest/instruments/InstrumentResource.java
+++ data-validation-rest/src/main/java/com/aeris/datavalidationrest/instruments/InstrumentResource.java
@@ -1,14 +1,32 @@
 package com.aeris.datavalidationrest.instruments;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/parameters")
 public class InstrumentResource {
     @Autowired
     private HttpServletRequest request;
+    @Autowired
+    private InstrumentDao instrumentDao;
+
+    @GetMapping
+    public List<Instrument> getAllInstruments() {
+        List<Instrument> instruments = instrumentDao.findAll();
+        return instruments;
+    }
+
+    @GetMapping(value = "/{id}")
+    public Optional<Instrument> getInstrumentById(@PathVariable String id) {
+        Optional<Instrument> instrument = instrumentDao.findById(id);
+        return instrument;
+    }
 }
