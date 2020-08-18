fe1f1f4d59c08ef12b412926b81da1a203ea6326
kande <dese_kande@hotmail.fr>
2020-08-18 12:49:57 +0200

    register select component

--- aeris-datavalidation-components/src/lib/modules/aeris-datavalidation-ui/submodules/aeris-datavalidation-inputs/index.js
+++ aeris-datavalidation-components/src/lib/modules/aeris-datavalidation-ui/submodules/aeris-datavalidation-inputs/index.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 
+import AerisDatavalidationSelect from "./components/submodules/aeris-datavalidation-selects/aeris-datavalidation-select";
 import AerisDatavalidationTimePicker from "./components/submodules/aeris-datavalidation-pickers/aeris-datavalidation-timepicker";
 import AerisDatavalidationDateMounthPicker from "./components/submodules/aeris-datavalidation-pickers/aeris-datavalidation-datemounthpicker";
 
+
 Vue.use(
+    AerisDatavalidationSelect,
     AerisDatavalidationTimePicker,
-    AerisDatavalidationDateMounthPicker
+    AerisDatavalidationDateMounthPicker,
 );
