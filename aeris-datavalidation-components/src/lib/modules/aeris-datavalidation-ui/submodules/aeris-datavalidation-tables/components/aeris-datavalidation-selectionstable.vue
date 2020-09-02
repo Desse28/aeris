f934efa72d7c6f8c8499f9da00b729f03bc969d0
dkande <desse.kande@ipsl.fr>
2020-09-02 11:47:31 +0200

    Set default selectionsTables

--- aeris-datavalidation-components/src/lib/modules/aeris-datavalidation-ui/submodules/aeris-datavalidation-tables/components/aeris-datavalidation-selectionstable.vue
+++ aeris-datavalidation-components/src/lib/modules/aeris-datavalidation-ui/submodules/aeris-datavalidation-tables/components/aeris-datavalidation-selectionstable.vue
@@ -1,33 +1,82 @@
 <template>
   <div>
-    <v-data-table
-        v-model="selected"
-        :headers="headers"
-        :items="desserts"
-        item-key="name"
-        show-select
-        :page.sync="page"
-        :items-per-page="itemsPerPage"
-        hide-default-footer
-        @page-count="pageCount = $event"
-        class="elevation-1"
-    >
-      <template v-slot:item.actions="{ item }">
-        <v-icon
-            small
-            class="mr-2"
-            @click="editItem(item)"
+      <template>
+        <v-data-table
+            :headers="headers"
+            :items="desserts"
+            class="elevation-1"
+            item-key="name"
+            show-select
+            v-model="selected"
+            hide-default-footer
+            :page.sync="page"
+            :items-per-page="itemsPerPage"
+            @page-count="pageCount = $event"
+            height="400"
         >
-          mdi-pencil
-        </v-icon>
-        <v-icon
-            small
-            @click="deleteItem(item)"
-        >
-          mdi-delete
-        </v-icon>
+          <template v-slot:top>
+            <v-toolbar flat color="white">
+              <v-toolbar-title>Seletions</v-toolbar-title>
+              <v-divider
+                  class="mx-4"
+                  inset
+                  vertical
+              ></v-divider>
+              <v-spacer></v-spacer>
+              <v-dialog v-model="dialog" max-width="500px">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                      color="primary"
+                      dark
+                      class="mb-2"
+                      v-bind="attrs"
+                      v-on="on"
+                  >New Item</v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">{{ formTitle }}</span>
+                  </v-card-title>
+
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field v-model="editedItem.name" label="Selection name"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field v-model="editedItem.lastUpdate" label="Last update"></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                    <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template v-slot:item.actions="{ item }">
+            <v-icon
+                small
+                class="mr-2"
+                @click="editItem(item)"
+            >
+              mdi-pencil
+            </v-icon>
+            <v-icon
+                small
+                @click="deleteItem(item)"
+            >
+              mdi-delete
+            </v-icon>
+          </template>
+        </v-data-table>
       </template>
-    </v-data-table>
     <div class="text-center pt-2">
       <v-pagination v-model="page" :length="pageCount"></v-pagination>
     </div>
@@ -39,10 +88,8 @@ export default {
   name: "aeris-datavalidation-selectionstable",
   data () {
     return {
+      dialog: false,
       selected: [],
-      page: 1,
-      pageCount: 0,
-      itemsPerPage: 10,
       headers: [
         {
           text: 'Selection',
@@ -50,31 +97,63 @@ export default {
           sortable: false,
           value: 'name',
         },
-        { text: 'last update', value: 'updateDate' },
-        { text: 'Action', value: 'action', sortable: false },
+        { text: 'Last update', value: 'lastUpdate' },
+        { text: 'Actions', value: 'actions', sortable: false },
       ],
       desserts: [
         {
-          name: 'selection1',
-          updateDate : '01/09/2020',
+          name: 'Selection1',
+          lastUpdate:  '01/09/2020',
         },
         {
           name: 'Selection2',
-          updateDate : '01/09/2020',
+          lastUpdate:  '01/07/2020',
         },
       ],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+        lastUpdate: 0,
+      },
+      defaultItem: {
+        name: '',
+        lastUpdate: 0,
+      },
+      page: 1,
+      pageCount: 0,
+      itemsPerPage: 5,
     }
   },
-  methods : {
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    },
+  },
+  watch: {
+    dialog (val) {
+      val || this.close()
+    },
+  },
+  methods: {
     editItem (item) {
       this.editedIndex = this.desserts.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialog = true
     },
+
     deleteItem (item) {
       const index = this.desserts.indexOf(item)
       confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
     },
+
+    close () {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
     save () {
       if (this.editedIndex > -1) {
         Object.assign(this.desserts[this.editedIndex], this.editedItem)
@@ -84,6 +163,7 @@ export default {
       this.close()
     },
   },
+
 }
 </script>
 
