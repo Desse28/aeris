5f89084275f4cffdc007774aa91dae8749192077
dkande <desse.kande@ipsl.fr>
2020-10-02 15:27:00 +0200

    Add unlink function handler

--- aeris-datavalidation-components/src/lib/modules/aeris-datavalidation-blocks/submodules/aeris-datavalidation-charts/components/aeris-datavalidation-chart.vue
+++ aeris-datavalidation-components/src/lib/modules/aeris-datavalidation-blocks/submodules/aeris-datavalidation-charts/components/aeris-datavalidation-chart.vue
@@ -20,7 +20,7 @@
               :displaylogo="false"
               :displayModeBar="true"
               :responsive="true"
-              :modeBarButtons=" modeBarButtons"
+              :modeBarButtons="modeBarButtons"
           >
           </Plotly>
         </v-col>
@@ -34,40 +34,18 @@
   import { Plotly } from 'vue-plotly'
 
   import {
+    blackTrashIconPath,
+    linkChartsIconPath,
+    unLinkChartsIconPath
+  } from "./../../../../aeris-datavalidation-common/icons/index"
+
+  import {
     AerisDataValidationServices,
   } from "./../../../../aeris-datavalidation-components"
 
   const SELECTION_BORDER_COLOR = 'rgb(84,217,27)'
   const SELECTION_BACKGROUND_COLOR = 'rgb(33, 150, 243)'
   const TARGET_SELECTION_BORDER_COLOR = 'rgb(255, 152, 0)'
-
-  const TRASH_ICON = {
-    'width': 500,
-    'height': 500,
-    'path': "M296 432h16a8 8 0 0 0 8-8V152a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 8v272a8 8 0 0 0 8 8zm-160 0h16a8 8 0 0 0 8-8V1" +
-            "52a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 8v272a8 8 0 0 0 8 8zM440 64H336l-33.6-44.8A48 48 0 0 0 264 0h-80a48 48 0 0" +
-            " 0-38.4 19.2L112 64H8a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h24v368a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V96h24a8 " +
-            "8 0 0 0 8-8V72a8 8 0 0 0-8-8zM171.2 38.4A16.1 16.1 0 0 1 184 32h80a16.1 16.1 0 0 1 12.8 6.4L296 64H152zM384 " +
-            "464a16 16 0 0 1-16 16H80a16 16 0 0 1-16-16V96h320zm-168-32h16a8 8 0 0 0 8-8V152a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 " +
-            "8v272a8 8 0 0 0 8 8z",
-  }
-
-  const LINK_ICON = {
-    'width': 500,
-    'height': 500,
-    'path':"M314.222 197.78c51.091 51.091 54.377 132.287 9.75 187.16-6.242 7.73-2.784 3.865-84.94 86.02-54.696 54.696-143" +
-        ".266 54.745-197.99 0-54.711-54.69-54.734-143.255 0-197.99 32.773-32.773 51.835-51.899 63.409-63.457 7.463-7.452 20" +
-        ".331-2.354 20.486 8.192a173.31 173.31 0 0 0 4.746 37.828c.966 4.029-.272 8.269-3.202 11.198L80.632 312.57c-32.755 " +
-        "32.775-32.887 85.892 0 118.8 32.775 32.755 85.892 32.887 118.8 0l75.19-75.2c32.718-32.725 32.777-86.013 0-118.79a83" +
-        ".722 83.722 0 0 0-22.814-16.229c-4.623-2.233-7.182-7.25-6.561-12.346 1.356-11.122 6.296-21.885 14.815-30.405l4.375-" +
-        "4.375c3.625-3.626 9.177-4.594 13.76-2.294 12.999 6.524 25.187 15.211 36.025 26.049zM470.958 41.04c-54.724-54.745-14" +
-        "3.294-54.696-197.99 0-82.156 82.156-78.698 78.29-84.94 86.02-44.627 54.873-41.341 136.069 9.75 187.16 10.838 10.838 " +
-        "23.026 19.525 36.025 26.049 4.582 2.3 10.134 1.331 13.76-2.294l4.375-4.375c8.52-8.519 13.459-19.283 14.815-30.405.621" +
-        "-5.096-1.938-10.113-6.561-12.346a83.706 83.706 0 0 1-22.814-16.229c-32.777-32.777-32.718-86.065 0-118.79l75.19-75.2c3" +
-        "2.908-32.887 86.025-32.755 118.8 0 32.887 32.908 32.755 86.025 0 118.8l-45.848 45.84c-2.93 2.929-4.168 7.169-3.202 11.1" +
-        "98a17.31 173.31 0 0 1 4.746 37.828c.155 10.546 13.023 15.644 20.486 8.192 11.574-11.558 30.636-30.684 63.409-63.457 54.73" +
-        "3-54.735 54.71-143.3-.001-197.991z",
-  }
 
   export default {
     name: "aeris-datavalidation-chart",
@@ -160,6 +138,7 @@
         modeBarButtons: [],
         currentSelection : null,
         currentParameters : [],
+        linkIcon: linkChartsIconPath,
       }
     },
     computed: {
@@ -643,14 +622,23 @@
               'select2d',
               {
                 name: "Delete selection",
-                icon: TRASH_ICON,
+                icon: {
+                  'width': 500,
+                  'height': 500,
+                  'path': blackTrashIconPath,
+                  fill: "#ed3131"
+                },
                 click: () => {
                   this.deleteTargetSelection()
                 }
               },
               {
                 name: "Link chart",
-                icon: LINK_ICON,
+                icon: {
+                  'width': 500,
+                  'height': 500,
+                  'path': this.linkIcon,
+                },
                 click: () => {
                   this.enableLinkedMode()
                 }
@@ -676,7 +664,17 @@
         }
       },
       enableLinkedMode : function() {
-        //
+        let path =  $('#' + this.chartId).find( "a[data-title='Link chart']").find("path")
+
+        if(this.isMainChart && this.linkIcon === linkChartsIconPath && path) {
+          this.linkIcon= unLinkChartsIconPath
+        } else  if(this.isMainChart && this.linkIcon === unLinkChartsIconPath) {
+          this.linkIcon= linkChartsIconPath
+        }
+
+        if(path)
+          path.attr('d', this.linkIcon)
+
         this.switchLinkedMode()
       },
       setLayout: function() {
@@ -691,7 +689,8 @@
           legend: this.getLayoutLegend(),
           xaxis : this.getLayoutXaxis("Time", "date"),
           yaxis : this.getLayoutYaxis("Values"),
-          shapes : this.selections
+          shapes : this.selections,
+          modebar: this.getLayoutModeBarConf()
         }
       },
       getLayoutTitle: function() {
@@ -773,6 +772,14 @@
           yanchor: "auto",
         }
       },
+      getLayoutModeBarConf : function () {
+        return {
+          orientation: 'h',
+          color: 'rgba(68, 68, 68, 0.3)',
+          bgcolor: 'rgba(255, 255, 255, 0.5)',
+          activecolor: "rgba(212, 10, 10, 0.7)",
+        }
+      }
     }
   }
 </script>
